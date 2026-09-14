5bae063e0ffece09c064c209f1a6805d80a81ec7
unofficialrehandhaka <itsofficial1510@gmail.com>
2026-09-14 20:56:12 +0530

    Update reasons.js

--- reasons.js
+++ reasons.js
@@ -15,13 +15,40 @@
 
 var REASONS = [
   "I love how your face lights up when you talk about philosophy.",
-  "I'm proud of how disciplined you've become with your UPSC prep.",
+  "I'm proud of how disciplined you've become with your workouts!",
   "I love the way you get so absorbed in your crochet projects.",
   "I'm proud of how far your Instagram has come, one post at a time.",
   "I love how you explain big ideas so simply, like they're obvious.",
   "I'm proud of you for choosing a hard path and sticking with it.",
   "I love your laugh when something actually catches you off guard.",
-  "I'm proud of how much you've grown this past year.",
+  "I'm proud of how much you've grown in last few years..",
   "I love that you notice things most people walk right past.",
-  "I'm proud of you, today and every day, meri jaan."
+  "I want to have kids with you someday:)",
+  "I'm proud of you, today and every day, meri jaan.",
+  "You are so so beautiful.",
+  "I feel blessed to just be in your presence.",
+  "To be chosen by you, feels like a divine blessing to me."
+  "You are such a cutie pie.",
+  "I'm inspired by you... in so many ways"
+  "You are so kind and loving.",
+  "I wanna grow old with you.",
+  "Your presence is calming.",
+  "Well, its an honour to be with you."
+  "Your hugs are very warm and cozy and filled with so much love",
+  "Your aura is veryy pretty!",
+  "You are just... amazing",
+  "You deserve all the love and happiness in the world",
+  "I want to marry you",
+  "You are mine",
+  "I want to travel the world with you.",
+  "Need some amazing experiences in life that I'll never dare to do unless you're with me",
+  "My heart belongs to you. Beats for you.",
+  "I am yours"
+  "I'm impressed by how amazing you are.",
+    "I'm amazed by how much you are able to handle.",
+    "I'm in awe of you",
+    "I get inspired by how u keep trying new things..",
+    "Your smile is so great",
+    "The sound of your laugh is exhilerating. (not ai writted:))",
+    "I was born to love you, Miss Budhraja"    
 ];
